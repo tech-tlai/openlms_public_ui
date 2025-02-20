00b51138a6f6ddaf76f68c423108f066b920d673
meera-thal <meera.salim@zaeemsolutions.com>
2025-02-20 12:56:58 +0530

    Update +layout.server.js

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,9 +2,10 @@ import '$lib/locals/i18n.js';
 
 export async function load({ locals, cookies }) {
 	const lang = locals.lang;
+	const userdetails = locals.user ? locals.user : {};
 	let session = {
 		language: lang || 'en',
-		user: { ...locals.user, name: cookies.get('name') }
+		user: { ...userdetails, name: cookies.get('name') }
 	};
 
 	return { session };
